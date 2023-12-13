9c5792bd8dd6d7d0c257a8584bd4ee5f16e3288a
almondnick <nickalmond14@gmail.com>
2023-12-13 14:39:50 -0600

    updated and changed route files to work properly, added uniqid via npm for adding a delete feature to the notes page, made db.json more accessible (was having some errors with retrieving data from json file)

--- routes/route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const noteRouter = require('./note');
-const app = express();
-
-app.use('/notes', noteRouter);
-
-module.exports = app;
--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
+const indexRoute = require('./routes/indexRoute');
+const noteRoute = require('./routes/noteRoute');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use(indexRoute);
+app.use(noteRoute);
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
